330c27ecbf3e28fe4417f6c2088698687c770da1
davidpoul1 <123439028+davidpoul1@users.noreply.github.com>
2023-11-11 11:58:30 +0530

    Update tb_full_adder.sv

--- tb_full_adder.sv
+++ tb_full_adder.sv
@@ -12,3 +12,33 @@ module tb_full_adder();
   end
 endmodule
 //testbench 
+
+
+//Another way to implement.
+/*
+module tb_full_adder();
+  wire sum;
+  reg a,b,c;
+  full_adder obj(a,b,c,sum);
+  initial begin 
+    $dumpfile("obj.vcd");
+    $dumpvars(1);
+    a  = 0;b = 0;c = 0;#100;
+    $finish;
+  end
+  always begin 
+    #40;
+    a = ~a;
+  end
+  
+  always begin 
+    #20;
+    b = ~b;
+  end
+  
+  always begin 
+    #10;
+    c = ~c;
+  end
+endmodule
+*/
